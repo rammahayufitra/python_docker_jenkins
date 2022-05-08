58a13ad8b6556effcd75015f5e7cac064f045fb8
ramma.fitra <rammafitracyt@gmail.com>
2022-05-08 15:54:52 +0700

    edit

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ camera = cv2.VideoCapture('rtsp://Anonymous:Anonymous@192.168.1.11:554')
 def getFrames(): 
     while True:
         success, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if not success:
             break
         else:
@@ -25,4 +25,4 @@ def getVideo():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='5000')
+    app.run(host='0.0.0.0',port=5000)
